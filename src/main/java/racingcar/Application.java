040e823daca71471de20a7c3f0e484a3a25a8586
sangmok0507 <baesangmok0507@gmail.com>
2025-10-27 21:00:40 +0900

    feat: 초기 자동차 경주 게임 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,64 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
+        String[] cars = carName.split(",");
+        List<List<Character>> resultOfMovement = new ArrayList<>();
+        for (int i = 0; i < cars.length; i++) {
+            List<Character> list = new ArrayList<>();
+            resultOfMovement.add(list);
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int count = Integer.parseInt(Console.readLine());
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < cars.length; j++) {
+                int number = Randoms.pickNumberInRange(0, 9);
+                if (number >= 4) {
+                    resultOfMovement.get(j).add('-');
+                }
+            }
+            for (int j = 0; j < cars.length; j++) {
+                System.out.print(cars[j] + " : ");
+                List<Character> list = resultOfMovement.get(j);
+                for (Character c : list) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        List<String> winner = new ArrayList<>();
+        int max = resultOfMovement.getFirst().size();
+        for (List<Character> r : resultOfMovement) {
+            if (max < r.size()) {
+                max = r.size();
+            }
+        }
+        for (int i = 0; i < cars.length; i++) {
+            List<Character> list = resultOfMovement.get(i);
+            if (max == list.size()) {
+                winner.add(cars[i]);
+            }
+        }
+        System.out.print("최종 우승자 : ");
+        if (winner.size() == 1) {
+            System.out.println(winner.getFirst());
+        } else {
+            for (int i = 0; i < winner.size() - 1; i++) {
+                System.out.print(winner.get(i) + ", ");
+            }
+            System.out.println(winner.getLast());
+        }
     }
 }
