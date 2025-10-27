e608a0366a35be2dfc89b5d8956907d32a7fdace
sangmok0507 <baesangmok0507@gmail.com>
2025-10-27 21:56:27 +0900

    refactor: 구현한 기능를 메서드로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,59 +7,106 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
 
-        final String delimiter = ",";
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carName = Console.readLine();
-        if (!carName.contains(delimiter)) {
-            throw new IllegalArgumentException("올바른 구분자를 입력하세요.");
+        String carName = getCarName();
+        isDelimiter(carName);
+
+        String[] cars = carName.split(",");
+        isLessThan5Letters(cars);
+
+        List<List<Character>> resultOfMovement = getResultOfMovement(cars);
+        int count = getCount();
+
+        playGame(count, cars, resultOfMovement);
+
+        List<String> winner = new ArrayList<>();
+        int max = getMax(resultOfMovement);
+        
+        findWinner(cars, resultOfMovement, max, winner);
+        printWinner(winner);
+    }
+
+    private static void playGame(int count, String[] cars, List<List<Character>> resultOfMovement) {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < count; i++) {
+            movementOfCar(cars, resultOfMovement);
+            printResultOfRace(cars, resultOfMovement);
         }
-        String[] cars = carName.split(delimiter);
+    }
+
+    private static void isLessThan5Letters(String[] cars) {
         for (String car : cars) {
             if (car.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
             }
         }
+    }
+
+    private static void isDelimiter(String carName) {
+        if (!carName.contains(",")) {
+            throw new IllegalArgumentException("올바른 구분자를 입력하세요.");
+        }
+    }
+
+    private static String getCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static int getCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private static List<List<Character>> getResultOfMovement(String[] cars) {
         List<List<Character>> resultOfMovement = new ArrayList<>();
         for (int i = 0; i < cars.length; i++) {
             List<Character> list = new ArrayList<>();
             resultOfMovement.add(list);
         }
+        return resultOfMovement;
+    }
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int count = Integer.parseInt(Console.readLine());
-
-        System.out.println("\n실행 결과");
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < cars.length; j++) {
-                int number = Randoms.pickNumberInRange(0, 9);
-                if (number >= 4) {
-                    resultOfMovement.get(j).add('-');
-                }
+    private static void movementOfCar(String[] cars, List<List<Character>> resultOfMovement) {
+        for (int j = 0; j < cars.length; j++) {
+            int number = Randoms.pickNumberInRange(0, 9);
+            if (number >= 4) {
+                resultOfMovement.get(j).add('-');
             }
-            for (int j = 0; j < cars.length; j++) {
-                System.out.print(cars[j] + " : ");
-                List<Character> list = resultOfMovement.get(j);
-                for (Character c : list) {
-                    System.out.print(c);
-                }
-                System.out.println();
+        }
+    }
+
+    private static void printResultOfRace(String[] cars, List<List<Character>> resultOfMovement) {
+        for (int j = 0; j < cars.length; j++) {
+            System.out.print(cars[j] + " : ");
+            List<Character> list = resultOfMovement.get(j);
+            for (Character c : list) {
+                System.out.print(c);
             }
             System.out.println();
         }
+        System.out.println();
+    }
 
-        List<String> winner = new ArrayList<>();
-        int max = resultOfMovement.getFirst().size();
-        for (List<Character> r : resultOfMovement) {
-            if (max < r.size()) {
-                max = r.size();
-            }
-        }
+    private static void findWinner(String[] cars, List<List<Character>> resultOfMovement, int max, List<String> winner) {
         for (int i = 0; i < cars.length; i++) {
             List<Character> list = resultOfMovement.get(i);
             if (max == list.size()) {
                 winner.add(cars[i]);
             }
         }
+    }
+
+    private static int getMax(List<List<Character>> resultOfMovement) {
+        int max = resultOfMovement.getFirst().size();
+        for (List<Character> r : resultOfMovement) {
+            if (max < r.size()) {
+                max = r.size();
+            }
+        }
+        return max;
+    }
+
+    private static void printWinner(List<String> winner) {
         System.out.print("최종 우승자 : ");
         if (winner.size() == 1) {
             System.out.println(winner.getFirst());
@@ -71,3 +118,4 @@ public class Application {
         }
     }
 }
+
